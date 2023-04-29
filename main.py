2ba5acde2e9c0c3d99935b002125f16f1351bf20
Garbis Ciftci <garbis.ciftci@gmail.com>
2023-04-29 02:42:13 +0200

    cleanup

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # main.py
-from app import init
-
-if __name__ == '__main__':
-    import sys
-    sys.exit(init.run())
-
 '''
     TODO: #18 Implement more Widgets
     TODO: #19 Implement testing
     TODO: #20 Implement modular Signal/Slot mechanics
 '''
+from app import init
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(init.run())
